7515cc3686fc952ad8ff72d3f5d949ca95f4aa2e
SaiRakindi <sai.rakindi3@gmail.com>
2025-08-21 00:04:20 +0530

    refactor: custom hooks implementation

--- src/components/CustomHook/CustomHook.jsx
+++ src/components/CustomHook/CustomHook.jsx
@@ -1,17 +1,5 @@
-import { useState } from "react";
 import "./styles.css";
-
-// ✅ Complete the custom hook below
-function useToggle(initialValue = false) {
-  // your code here
-  const [isOn, setIsOn] = useState(initialValue);
-
-  const toggle = () => {
-    setIsOn((prev) => !prev);
-  };
-
-  return [isOn, toggle];
-}
+import { useToggle } from "../../hooks/useToggle";
 
 export default function CustomHook() {
   // ✅ Use the custom hook inside this component
