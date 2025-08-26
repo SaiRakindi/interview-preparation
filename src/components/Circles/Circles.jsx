00585ff96fe209f9ef4a984a1535d88f57789e9b
SaiRakindi <sai.rakindi3@gmail.com>
2025-08-26 23:42:33 +0530

    styles: cirlces styles

--- src/components/Circles/Circles.jsx
+++ src/components/Circles/Circles.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./styles.css";
 
 const Circles = () => {
-  return <div>Circles</div>;
+  const [circles, setCircles] = useState([]);
+  const [redoCircles, setRedoCircles] = useState([]);
+
+  return (
+    <div className="circle-drawer">
+      <div
+        data-testid="drawing-area"
+        className="drawing-area"
+        onClick={handleDraw}
+      >
+        {circles.map((circle) => (
+          <div style={{ left: circle.x, top: circle.y }}></div>
+        ))}
+      </div>
+      <div className="buttons">
+        <button className="undo-btn" onClick={handleUndo}>
+          Undo
+        </button>
+        <button className="redo-btn" onClick={handleRedo}>
+          Redo
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Circles;
