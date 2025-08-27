79a7cb86df0a28001a98342d3035093b634919ea
SaiRakindi <sai.rakindi3@gmail.com>
2025-08-27 23:43:11 +0530

    feat: guess the number initial code

--- src/components/GuessTheNumber/GuessTheNumber.jsx
+++ src/components/GuessTheNumber/GuessTheNumber.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
 const GuessTheNumber = () => {
-  return <div>GuessTheNumber</div>;
+  // Function to handle guess checking
+  const handleGuess = () => {};
+
+  // Function to reset the game
+  const resetGame = () => {};
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: "50px 0",
+      }}
+    >
+      <h2>Guess the Number</h2>
+      <input
+        placeholder="Enter a number between 1 and 100"
+        style={{ width: "300px", padding: "5px" }}
+        id="guess-input"
+      />
+    </div>
+  );
 };
 
 export default GuessTheNumber;
